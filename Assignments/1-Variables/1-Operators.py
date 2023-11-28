d97306aa1308f1c673ab398a9c12e7cea7dfbaa9
damianbrands <damian@sparkles.io>
2023-11-28 15:42:55 +0100

    Operators

---
+++ Assignments/1-Variables/1-Operators.py
@@ -0,0 +1,34 @@
+# Starting note:
+# To print something in the terminal use print()
+
+# 1-Operators
+# Just like in math you can use operators in python.
+# For example: 1 + 1 = 2
+# It will automatically be calculated and if you print it, the result will be printed so 2.
+# If you run the file you will see that the first printed thing is '2'.
+
+print(1 + 1)
+
+# Here is a list of the basic operators:
+# '+' add two numbers so 1 + 1 = 2
+# '-' subtract two numbers so 1 - 1 = 0
+# '*' multiply two numbers so 2 * 4 = 8
+# '/' divide two numbers so 9 / 3 = 3
+# '**' exponential so 3 ** 2 = 9
+# '%' modulus is the remaining amount after dividing as far as possible with whole numbers.
+#       For example: 13 % 5 = 3
+#       The furthest dividing by 5 goes with whole numbers is 10 this leaves us with 3 left. (The number % gives us)
+
+print(1 - 1)
+print(2 * 4)
+print(9 / 3)
+print(3 ** 2)
+print(13 % 5)
+
+# the order of calculations is according to PEMDAS:
+
+print(5 * 10 % 20)
+
+# If you run the file you will see that all the numbers are in line with the earlier mentioned examples
+# Feel free to try your own ideas out below, you can also mix stuff together.
+# Make sure to rerun the file after writing things down.
