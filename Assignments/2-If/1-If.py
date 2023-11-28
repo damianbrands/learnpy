e0360b137d1cb98352b981eb2ec0ee92e2d70320
Junmii <joys.it.projects@gmail.com>
2023-11-28 16:07:17 +0100

    Added 3 assignments to 'If-statements'

---
+++ Assignments/2-If/1-If.py
@@ -0,0 +1,42 @@
+# Hello! In this chapter you will be learning about If-Else statements and Inputs.
+
+# If statements:
+# An If statement is a statement to check a condition, and execute something if the condition is true.
+
+def firstassignment():
+    # For this assignment you want to check if the number of variable B is greater than the number of variable A.
+    a = 4
+    b = 9
+
+    # Add a condition between the parentheses after 'if'
+    if ():
+        print("B is greater than A")
+
+    # Expected output: "B is greater than A"
+
+def secondassignment():
+    # For this assignment you need to check if variable B is the same as variable A.
+    a = "Hello World!"
+    b = "Hello World!"
+
+    # Add a condition between the parentheses after 'if'
+    if ():
+        print("B and A are the same")
+
+    # Expected output: "B and A are the same"
+
+def thirdassignment():
+    # Now we want to check if B is greater than A, but smaller than C
+    a = 2
+    b = 5
+    c = 10
+
+    # Add a condition between the parentheses after 'if'
+    if ():
+        print("B is greater than A, but smaller than C")
+
+    # Expected output: "B is greater than A, but smaller than C"
+
+firstassignment()
+secondassignment()
+thirdassignment()
